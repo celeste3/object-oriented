9c028022158ec6565e95963a56e4605affe7c656
Celeste Whitaker <abqcc3@gmail.com>
2020-01-30 08:55:49 -0700

    Added ValidateUuid and comment section below

--- sql/author.sql
+++ sql/author.sql
@@ -1,11 +1,22 @@
 <?php
 
 class author {
+	use ValidateUuid;
+	/**
+	* authorId is the primary key
+	*
+	**/
+
 	private $authorId;
+
 	private $authorAvatarUrl;
+
 	private $authorActivationToken;
+
 	private $authorEmail;
+
 	private $authorHash;
+
 	private $authorUsername;
 
 

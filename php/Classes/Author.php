e821123fbaba93601178fa1fd000ef34f50e0ab3
Celeste Whitaker <abqcc3@gmail.com>
2020-02-08 23:25:10 -0700

    Added private $authorhash unsure if syntax is correct. Doc Block to reference DDL

--- php/Classes/Author.php
+++ php/Classes/Author.php
@@ -20,18 +20,44 @@ class Author {
 
 	private $authorId;
 
+
 	private $authorAvatarUrl;
 
 	private $authorActivationToken;
 
 	private $authorEmail;
 
-	private $authorHash;
+	private $authorHash; $hash = password_hash("password", PASSWORD_ARGON2I, ["time_cost" => 7]);
 
 	private $authorUsername;
 
-
-
 }
+
+
+
+
+//DDL for Assignment
+
+//create table author(
+
+//	 authorId binary(16) not null,
+
+//  authorActivationToken char(32),
+
+//  authorAvatarUrl varchar(255),
+
+//  authorEmail varchar(128) not null,
+
+//  authorHash char(97) not null,
+
+//  authorUsername varchar(32) not null,
+
+//  unique(authorEmail),
+
+//  unique(authorUsername),
+
+//  primary key(authorId)
+
+//
 
 ?>
